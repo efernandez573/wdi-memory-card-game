73ace1837a752fffbea752ab55135a6c00d46904
Jessy Resende <jessyresende@Jessys-MBP.fios-router.home>
2018-10-05 19:50:22 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log (" User flipped " + cardFour + " User flipped " + cardThree);
+var cardOne = document.getElementById("first-card")
+cardOne.addEventListener("click",()=>{
+	console.log("Clicked first card")
+	cardOne.src= "images/queen-of-diamonds.png"
+}) 
