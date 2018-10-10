dab6c8138d67caf37e63c9c07f178bfa665a7200
efernandez573 <elsiefernandez@gmail.com>
2018-10-10 16:26:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cards.push('queen'), cards.push('king')];
+var cards = [
+	{
+		rank:'queen',
+		suit:'hearts',
+		cardImage: 'images/queen-of-hearts.png'},
+{
+		rank:'queen',
+		suit: 'diamonds',
+		cardImage:'images/queen-of-diamonds.png'},
+{
+ 		rank:'king',
+		suit: 'hearts',
+		cardImage:'images/king-of-hearts.png'},
+{
+		rank:'king',
+		suit: 'diamonds',
+		cardImage:'images/king-of-diamonds.png'}
+];
+
+var cardsInPlay = cards[1].rank
 var cardOne = cards[0]
-{console.log("User flipped queen");} 
-var cardTwo = cards[2] 
-{console.log("User flipped king");}
+function checkForMatch(){
 
-if (cards[0]===cards[1] && cardsInPlay.length=== 2) 
-	{alert("You found a match!")}
-else 
-	{alert("Sorry, try again.")}
+	if (cardsInPlay[0] === cardsInPlay[1] && cardsInPlay.length === 2){
+		alert("You found a match!")
+	}
+else if(cardsInPlay[0] === cardsInPlay[2] && cardsInPlay.length === 2){
+		alert("Sorry, try again.")
+	}
+}
+function flipCard(cards){
 
 
+};
+	console.log("User flipped " + cards[0].rank)
+
+console.log(cards[0].cardImage)
+
+console.log(cards[0].suit)
+console.log("User flipped " + cards[2].rank)
+console.log(cards[2].cardImage)
+console.log(cards[2].suit)
